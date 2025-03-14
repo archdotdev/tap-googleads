e107e850e5c547e66dbe990e84344df1b690cf55
Pat Nadolny <patnadolny@gmail.com>
2025-03-14 11:02:16 -0400

    clean up default schema fields

--- tap_googleads/custom_query_stream.py
+++ tap_googleads/custom_query_stream.py
@@ -110,11 +110,8 @@ class CustomQueryStream(ReportsStream):
             # {'metrics': {'costMicros': 1000000}} which gets converted to metrics__costMicros
             field_name = "__".join([humps.camelize(i) for i in field.split(".")])
             local_json_schema["properties"][field_name] = field_value
-        # These are always present in the response
+        # This is always present in the response
         local_json_schema["properties"]["customer_id"] = {"type": ["string", "null"]}
-        local_json_schema["properties"]["campaign__resourceName"] = {
-            "type": ["string", "null"]
-        }
         return local_json_schema
 
     def _cast_value(self, key: str, value: Any) -> Any:
