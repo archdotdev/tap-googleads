ee9780a7e5d15872c0fabd5e0e25ef55654368c8
Pat Nadolny <patnadolny@gmail.com>
2025-03-17 14:30:55 -0400

    Update tap_googleads/tap.py

--- tap_googleads/tap.py
+++ tap_googleads/tap.py
@@ -160,5 +160,5 @@ class TapGoogleAds(Tap):
         streams = [stream_class(tap=self) for stream_class in STREAM_TYPES]
         if self.config.get("enable_click_view_report_stream"):
             streams.append(ClickViewReportStream(tap=self))
-        streams.extend(CustomQueryStream(tap=self, custom_query=q) for q in self.config["custom_queries_array"])
+        streams.extend(CustomQueryStream(tap=self, custom_query=q) for q in self.config["custom_queries"])
         return streams
